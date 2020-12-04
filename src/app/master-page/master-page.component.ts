fbd90f269e045a78b7a2f6a345993e83c9f02d05
RUnroe <ryanunroe@gmail.com>
2020-12-03 23:58:16 -0700

    Moved page options to own file to be used globally

--- src/app/master-page/master-page.component.ts
+++ src/app/master-page/master-page.component.ts
@@ -1,21 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GlobalOptions } from '../options';
 @Component({
   selector: 'app-master-page',
   templateUrl: './master-page.component.html',
   styleUrls: ['./master-page.component.scss']
 })
 export class MasterPageComponent implements OnInit {
-  pages = [
-    {name: "Introduction", pageNum: 1},
-    {name: "What You Will Need", pageNum: 2},
-    {name: "Setup - Imaging", pageNum: 3},
-    {name: "Setup - Booting", pageNum: 4},
-    {name: "Overview", pageNum: 5},
-    {name: "UI Tour", pageNum: 6},
-    {name: "Explore", pageNum: 7}
-  ]
-  constructor() { }
+  pages = GlobalOptions.pages;
+  constructor() { 
+    console.log(this.pages);
+    console.log(GlobalOptions);
+  }
 
   ngOnInit(): void {
   }
---
+++ src/app/options.ts
@@ -0,0 +1,11 @@
+export class GlobalOptions  {
+    public static pages = [
+        {name: "Introduction", pageNum: 1},
+        {name: "What You Will Need", pageNum: 2},
+        {name: "Setup - Imaging", pageNum: 3},
+        {name: "Setup - Booting", pageNum: 4},
+        {name: "Overview", pageNum: 5},
+        {name: "UI Tour", pageNum: 6},
+        {name: "Explore", pageNum: 7}
+    ]
+}
